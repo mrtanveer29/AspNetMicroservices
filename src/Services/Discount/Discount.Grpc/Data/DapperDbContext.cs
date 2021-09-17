5ebeca414ce255ef81274547f1977566ec2c5e4a
tanveer_h <tanveer.h@bitmascot.com>
2021-09-17 15:12:13 +0600

    Discount GRPC

---
+++ src/Services/Discount/Discount.Grpc/Data/DapperDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Discount.Grpc.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Data
+{
+    public class DapperDbContext
+    {
+        private IDatabaseSettings _databaseSettings;
+        public DapperDbContext(IDatabaseSettings settings)
+        {
+            this._databaseSettings = settings;
+        }
+
+        public NpgsqlConnection Getconnection()
+        {
+            return new NpgsqlConnection(_databaseSettings.ConnectionString);
+        }
+    }
+}
